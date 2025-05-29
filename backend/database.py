3be1952c26134b4c2e3e8624f1e29f30d7059803
Keeobom <melunatam4@gmail.com>
2025-05-29 02:53:00 +0100

    git_and_local_matching

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://log_db_swpc_user:FaNOI0C3OiMNSkbb9d19h0AwGo1jbg2b@dpg-d0rqkb15pdvs738th5f0-a.oregon-postgres.render.com/log_db_swpc"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
